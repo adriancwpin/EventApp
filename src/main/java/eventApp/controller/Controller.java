ddd1adcb0d5d5d6bc16013e0175bf4024af5a25d
adriancwpin <adriancwpin8@gmail.com>
2026-03-25 00:59:07 +0000

    initialise all controllers

--- src/main/java/eventApp/controller/Controller.java
+++ src/main/java/eventApp/controller/Controller.java
@@ -1,4 +1,39 @@
 package eventApp.controller;
 
-public class Controller{
+import java.util.Collection;
+
+public abstract class Controller{
+    /**
+     * Checks if the currently logged-in user has Admin privileges.
+     * @return true if the user is an admin, false otherwise.
+     */
+
+    protected boolean checkCurrentUserIsAdmin(){
+        return false;
+    }
+
+    protected boolean checkCurrentUserIsGuest(){
+        return false;
+    }
+
+    protected boolean checkCurrentUserIsStudent(){
+        return false;
+    }
+
+    protected boolean checkCurrentUserIsEntertainmentProvider(){
+        return false;
+    }
+
+    /**
+     * Display a menu of options to the user and capture their selection.
+     *
+     * @param <T> a generic to show the type of elements in the option collection
+     * @param options a collection of items to be displayed as menu choices
+     * @param prompt the text message displayed to the users before selection
+     * @return the index of the selected choice or 0 if no selection was made
+     */
+
+    protected <T> int selectFromMenu(Collection<T> options,  String prompt){
+        return 0;
+    }
 }
--- src/main/java/eventApp/controller/MenuController.java
+++ src/main/java/eventApp/controller/MenuController.java
@@ -1,4 +1,68 @@
 package eventApp.controller;
 
-public class MenuController {
+import eventApp.external.PaymentSystem;
+import eventApp.external.VerificationService;
+import eventApp.model.*;
+import eventApp.view.*;
+import eventApp.enums.*;
+import eventApp.external.*;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class MenuController extends Controller {
+
+    //All controllers 1-1 relationship
+    private UserController userController;
+    private EventPerformanceController eventPerformanceController;
+    private BookingController bookingController;
+
+    //Shared collections
+    private Collection<User> users;
+    private Collection<Event> events;
+    private Collection<Booking> bookings;
+    private Collection<Performance>performances;
+
+    private User currentUser;
+
+    private View view;
+    private PaymentSystem paymentSystem;
+    private VerificationService verificationService;
+
+    public MenuController(View view, PaymentSystem paymentSystem, VerificationService verificationService){
+        this.view = view;
+        this.paymentSystem = paymentSystem;
+        this.verificationService = verificationService;
+
+        //initialise shared collections
+        this.users = new ArrayList();
+        this.events = new ArrayList();
+        this.bookings = new ArrayList();
+        this.performances = new ArrayList();
+        this.currentUser = null; // no one logged in at start
+
+        //initialise all controllers and shared resources
+        this.userController = new UserController(users, currentUser, view, verificationService);
+        this.eventPerformanceController = new EventPerformanceController(events, performances, currentUser, view, paymentSystem);
+        this.bookingController = new BookingController(bookings, performances, currentUser, view, paymentSystem);
+    }
+
+    public void mainMenu() {}
+
+    private boolean handleGuestMainMenu(){
+        return false;
+    }
+
+    private boolean handleStudentMainMenu(){
+        return false;
+    }
+
+    private boolean handleEntertainmentProviderMainMenu(){
+        return false;
+    }
+
+    private boolean handleAdminStaffMainMenu(){
+        return false;
+    }
 }
