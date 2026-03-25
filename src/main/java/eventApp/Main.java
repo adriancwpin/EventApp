048d06ca84b7f06c84fbe5754cb76e28ace62854
adriancwpin <adriancwpin8@gmail.com>
2026-03-25 11:41:56 +0000

    Implemented main.java

--- src/main/java/eventApp/Main.java
+++ src/main/java/eventApp/Main.java
@@ -1,9 +1,17 @@
 package eventApp;
 
+import eventApp.controller.MenuController;
+import eventApp.external.*;
 import eventApp.view.*;
 
 public class Main {
     public static void main(String[] args) {
         View view = new TextUserInterface();
+        PaymentSystem paymentSystem = new MockPaymentSystem();
+        VerificationService verificationService = new MockVerificationService();
+
+        MenuController menuctrl = new MenuController(view, paymentSystem, verificationService);
+
+        menuctrl.mainMenu();
     }
 }
